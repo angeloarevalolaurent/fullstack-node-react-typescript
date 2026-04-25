81cdd1001b4ff885181f925c699ffe09a81e3fdf
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-04-25 11:07:55 -0500

    /client/ Trabajando con el Layout

--- client/src/layouts/Layouts.tsx
+++ client/src/layouts/Layouts.tsx
@@ -4,9 +4,18 @@ export default function Layouts() {
 
   return (
     <>
-      <div>Desde Layout</div>
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-10">
+            <h1 className="text-4xl font-extrabold text-white">
+                Administrados de Productos
+            </h1>
+        </div>
+      </header>
 
-      <Outlet />
+      <main className="mt-10 mx-auto w-full max-w-6xl rounded-2xl bg-white p-6 sm:p-8 md:p-10 shadow-xl border border-gray-100">
+            <Outlet />
+      </main>
+      
     </>
   )
 }
