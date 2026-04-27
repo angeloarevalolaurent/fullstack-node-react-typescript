8f46da3ef7c21a557308a2d241785e0a913368d1
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-04-27 10:41:10 -0500

    /client/  Validando los Datos con Valibot

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,3 +1,7 @@
+import { safeParse } from "valibot"
+import { DraftProductSchema } from "../types"
+
+
 
 type ProductData = {
     [k: string]: FormDataEntryValue
@@ -5,6 +9,21 @@ type ProductData = {
 
 
 export async function addProduct(data:ProductData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        console.log(result);
+        
+        if (result.success) {
+                
+        } else {
+            throw new Error('Invalid data')
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
     
