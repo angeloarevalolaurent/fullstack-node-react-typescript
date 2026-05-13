eabd1ff3480db1cf32aae42dad16ef3d23ab5c4f
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-13 17:28:23 -0500

    Agregando la Disponibilidad en edicion

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -39,6 +39,16 @@ export async function action({request, params}: ActionFunctionArgs) {
   }
 }
 
+
+
+const availabilityOptions = [
+   { name: 'Disponible', value: true},
+   { name: 'No Disponible', value: false}
+]
+
+
+
+
 export default function EditProduct() {
 
   const product = useLoaderData() as Product
@@ -96,6 +106,26 @@ export default function EditProduct() {
                   defaultValue={product.price}
               />
           </div>
+
+
+          <div className="mb-4">
+                <label
+                    className="text-gray-800"
+                    htmlFor="availability"
+                >Disponibilidad:</label>
+                <select 
+                    id="availability"
+                    className="mt-2 block w-full p-3 bg-gray-50"
+                    name="availability"
+                    defaultValue={product?.availability.toString()}
+                >
+                    {availabilityOptions.map(option => (
+                      <option key={option.name} value={option.value.toString()}>{option.name}</option>
+                    ))}
+                </select>
+            </div>
+
+
           <input
             type="submit"
             className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
